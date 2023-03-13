08b97c639c513b0f414e94af50a8523027245f11
comand3rwin <50809285+comand3rwin@users.noreply.github.com>
2023-03-13 16:50:51 +0100

    Add files via upload

---
+++ elefante.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main ()
+{ 
+    int x;
+    int y;
+ 
+    printf("i nserisci un numero");
+    scanf("%d" , &x);
+    printf(" inserisci un altro numero");
+    scanf("%d" , &y);
+    printf("il numero e %d,%d\n" , x , y);
+}
